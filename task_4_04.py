e1a1bf48970c7ac547d7c6d72e56a8dd53c697ea
shamrov <d_shamrov@mail.ru>
2022-06-07 21:46:46 +0300

    Complete task_4_04

---
+++ task_4_04.py
@@ -0,0 +1,8 @@
+import utils
+
+url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+print(utils.currency_rates(url, 'USd'))
+print(utils.currency_rates(url, 'GBP2'))
+
+print(utils.currency_rates_advanced(url, 'USd'))
+print(utils.currency_rates_advanced(url, 'GBP2'))
